b126ff030feca85d8e1339c111c4bafab66023bf
Roha Pathan <rohaabropathan@gmail.com>
2026-03-16 15:43:32 +0500

    fixed auth, broke down auth into client,server and middleware and removed manual inert into public.users table (trigger will be used instead)

--- src/utils/supabase/client.ts
+++ src/utils/supabase/client.ts
@@ -1,6 +1,8 @@
-import { createClient } from "@supabase/supabase-js";
 
-export const supabase = createClient(
-  "https://dfsutzuegywynbzmzvwz.supabase.co",
-  "sb_publishable_O8heNzmt_5skljH2Rew8YA_kpoLZnke"
+
+import { createBrowserClient } from "@supabase/ssr";
+
+export const supabase = createBrowserClient(
+  process.env.NEXT_PUBLIC_SUPABASE_URL!,
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
 );
